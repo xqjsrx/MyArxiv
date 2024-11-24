73f402c8521814b90f65988e8043da4d78758645
Q Wyatt <77832105+xqjsrx@users.noreply.github.com>
2024-11-24 20:08:23 +0800

    Update sort_papers.py

--- sort_papers.py
+++ sort_papers.py
@@ -45,17 +45,20 @@ if latest_day_container:
                 scored_articles.sort(key=lambda x: x[0], reverse=True)
                 print(f"Sorted articles for category {category_name}: {[article.find('summary').text.strip() for _, article in scored_articles]}")
                 
+                # # 将排序后的论文重新插入到类别下
+                # for score, article in scored_articles:
+                #     category.insert_after(article)
+                #     print(f"Inserted article with score {score}: {article.find('summary').text.strip()}")
+
+
                 # 获取 details-content 容器
                 details_content = category.find('div', class_='details-content')
-                if details_content:
-                    # 清空原有的文章
-                    for child in details_content.find_all('article'):
-                        child.decompose()
-                    
+                print(details_content)
+                if details_content:      
                     # 将排序后的论文重新插入到 details-content 容器中
                     for score, article in scored_articles:
                         details_content.append(article)
-                        # print(f"Inserted article with score {score}: {article.find('summary').text.strip()}")
+                        print(f"Inserted article with score {score}: {article.find('summary').text.strip()}")
 
 # 将修改后的 HTML 内容写回文件
 with open("target/index.html", 'w') as f:

1c6d2c6ff1d2c86cf54384af8e20badd24eee59a
Q Wyatt <77832105+xqjsrx@users.noreply.github.com>
2024-11-24 17:24:31 +0800

    Update inject_html.py

--- inject_html.py
+++ inject_html.py
@@ -13,7 +13,7 @@ with open("target/index.html", 'r') as f:
 soup = BeautifulSoup(html_content, 'html.parser')
 
 # 创建一个字典，用于快速查找论文的评分、理由和摘要
-papers_dict = {paper['id']: paper for paper in evaluated_papers if all(key in paper for key in ['id', 'score', 'reason', 'summary'])}
+papers_dict = {paper['id']: paper for paper in evaluated_papers if all(key in paper for key in ['id', 'score', 'reason', 'summary', 'abstract'])}
 
 # 查找所有文章的详细信息部分
 articles = soup.find_all('details', class_='article-expander')
@@ -28,6 +28,7 @@ for article in articles:
         score = paper_info.get('score', 'N/A')
         reason = paper_info.get('reason', 'N/A')
         summary = paper_info.get('summary', 'N/A')
+        abstract = paper_info.get('abstract', 'N/A')
         
         # 更新标题，将评分放在最前面，并用 <span class="chip"> 包裹
         title_summary = article.find('summary', class_='article-expander-title')
@@ -40,15 +41,15 @@ for article in articles:
         # 处理摘要
         first_summary_box = article.find('div', class_='article-summary-box-inner')
         if first_summary_box:
-            abstract_span = soup.new_tag('span', **{'class': 'chip'})
-            abstract_span.string = 'abstract:'
+            abstract_chip = soup.new_tag('span', **{'class': 'chip'})
+            abstract_chip.string = 'abstract'
             abstract_text = soup.new_tag('span')
-            abstract_text.string = f' {paper_info["abstract"]}'
+            abstract_text.string = f': {abstract}'
             
             # 替换第一个 span 的内容
             first_span = first_summary_box.find('span')
             if first_span:
-                first_span.replace_with(abstract_span)
+                first_span.replace_with(abstract_chip)
                 first_summary_box.insert(1, abstract_text)
         
         # 创建新的理由和摘要元素
